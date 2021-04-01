286e4f43a8d1c50febc561fd3a91317c1d4655e8
Autumn-Coding <78398232+Autumn-Coding@users.noreply.github.com>
2021-04-01 19:42:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+var questions = document.getElementsByClassName("question");
+
+for (i = 0; i < questions.length; i++) {
+  questions[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } 
+  });
+}
